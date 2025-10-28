ae6a4effb88a18e95d52c90251e4692d80398414
Edax Ucles <filanderuclez@gmail.com>
2025-10-28 15:03:20 -0600

    Configure for trakking.app custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://f1lander.github.io',
-  base: '/trakking-website/',
+  site: 'https://trakking.app',
 });
